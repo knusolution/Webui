9dec7123cb17d3a31d99a1fd1e838a6419805cd2
smin0820 <97029953+smin0820@users.noreply.github.com>
2023-11-07 21:05:44 +0900

    [Feat] 검색창 마크업 및 스타일링

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '@components/header/Header'
+import Search from '@components/search/Search';
 
 
 
@@ -9,8 +10,8 @@ export default function Admin() {
     return (
         <>
             <Header></Header>
+            <Search></Search>
             
-    
         </>
     );
 }
