ec8fa8e2dda6ce0ac8f090567ec99d4efa13d0c5
DWK3732 <101383056+DWK3732@users.noreply.github.com>
2023-11-16 13:42:30 +0900

    [Feat] 정보수정 페이지 + 정보수정 확인모달창 마크업 및 스타일링

---
+++ src/components/modify/Modify.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ModifyBoardContainer from './containers/ModifyBoardContainer';
+
+const Modify = () => {
+  return (
+    <ModifyBoardContainer />
+  );
+};
+
+export default Modify;
