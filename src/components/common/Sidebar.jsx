c0777d7b3f0b9c7a35150910041fa669db14182b
smin0820 <97029953+smin0820@users.noreply.github.com>
2023-11-22 14:49:05 +0900

    [Feat] 시스템 사이드바 마크업

---
+++ src/components/common/Sidebar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const SidebarUL = styled.ul`
+    list-style: none;
+    padding-left: 1rem;
+    position: absolute;
+    top: 25%;
+    li {
+        font-size: 13px;
+        margin-bottom: 10px;
+    }
+`
+
+const SNavLink = styled(NavLink)`
+    text-decoration: none;
+    color: black;
+    text-decoration: none;
+    &.active {
+        color: #4DBDE5;
+    }
+`
+
+
+export default function Sidebar() {
+    const systemname = [
+        {name: "공지사항", path: "/admin"},
+        {name: "경상북도 지도기반 통계정보시스템", path: "/system"},
+        {name: "경상북도 xxx 시스템", path: "/system/0"}
+    ];
+    return (
+      <SidebarUL>
+        {systemname.map((e, i) => (
+          <li key={i}>
+            <SNavLink to={e.path}>
+              {e.name}
+            </SNavLink>
+          </li>
+        ))}
+      </SidebarUL>
+    );
+}
+
