d7d0e19b3c96f3cff40b5b5bf750d6bb9436087d
DWK3732 <ugi9297@naver.com>
2023-11-29 02:18:04 +0900

    [Feat] 관리자 페이지의 페이지 기능 추가

--- src/components/admin/Admin.jsx
+++ src/components/admin/Admin.jsx
@@ -1,31 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import AdminHeaderContainer from '@components/admin/containers/AdminHeaderContainer'
 import AdminSearchContainer from '@components/admin/containers/AdminSearchContainer';
-import AdminBoardContainer from '@components/admin/containers/AdminBoardContainer';
 import Sidebar from '@components/common/Sidebar';
-import AdminArticleService from '@components/admin/containers/AdminArticleService';
-
+import AdminContentContainer from './containers/AdminContentContainer';
 
 
 
 
 export default function Admin() {
-    const [articles, setArticles] = useState([]);
-
-    useEffect(() => {
-        AdminArticleService.fetchArticles()
-            .then(articleData => {
-                setArticles(articleData.data);
-            })
-            .catch(error => console.error('Articles 요청 오류:', error));
-    }, []);
 
     return (
         <>
             <AdminHeaderContainer></AdminHeaderContainer>
             <Sidebar></Sidebar>
             <AdminSearchContainer></AdminSearchContainer>
-            <AdminBoardContainer title="[전체 게시판]" data={articles}></AdminBoardContainer>
+            <AdminContentContainer></AdminContentContainer>
         </>
     );
 }
--- src/components/admin/containers/AdminArticleService.jsx
+++ src/components/admin/containers/AdminArticleService.jsx
@@ -2,11 +2,13 @@
 import axios from 'axios';
 
 const AdminArticleService = {
-    fetchArticles: async () => {
+    fetchArticles: async (page) => {
         try {
-            const response = await axios.get('http://localhost:8080/wait-article');
+            const response = await axios.get('http://localhost:8080/wait-article', {
+                params: { page }
+            });
             console.log('Success fetching articles:', response);
-            return response;
+            return response.data;
         } catch (error) {
             console.error('Error fetching articles:', error);
             throw error;
